16f98173db7a417b5eddd7ccde947dd4acaf4a39
jbizLT <pozolotusiko@mail.ru>
2022-09-14 22:14:56 +0300

    first commit

---
+++ JavaSPbPU/lab_2/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -0,0 +1,24 @@
+package problemset2_2;
+
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        System.out.println("_╱▔▔╲╱▔▔▔▔╲╱▔▔╲_");
+        System.out.println("╲ ▔╲  *  *  ╱▔ ╱");
+        System.out.println(" ▔╲╱▏ ╱▔▔╲ ▕╲╱▔ ");
+        System.out.println("   |  ▏||▕  |   ");
+        System.out.println("    ╲ ╲__╱ ╱    ");
+        System.out.println();
+        System.out.println("┈┈┈┈┈┈┈╱▏▕╲");
+        System.out.println("┈┈┈┈┈╱▏▏▏▕▕");
+        System.out.println("┈┈┈┈┈▏▏▏▏▕▕");
+        System.out.println("┈┈▕╲┈▏▏╲╲╱▔▔╲");
+        System.out.println("┈┈▕▕-╲╲╱|╱▔▔▔");
+        System.out.println("┈┈┈╲▔▔_╱▔");
+        System.out.println("┈┈┈┈▔▔▉╭━┓");
+        System.out.println("┈┈┈╱▔▔┊╯╭┃");
+        System.out.println("╱▔▔┈╭▅┓┊┳╯");
+        System.out.println("|▏╯┈┗━╯┊╰");
+        System.out.println("╲━╯┊┊┊┊┊┊");
+        System.out.println("┈▔▔▔╲┊┊┊");
+    } 
+}
