7c8c3d69aa55b7955860f2e3d5c9c057fdac810c
jbizLT <pozolotusiko@mail.ru>
2022-11-17 15:58:08 +0300

    fixed lab_11

--- Oleg/Java/lab_11/ArrayListsEx/src/com/example/ArrayListEx1.java
+++ Oleg/Java/lab_11/ArrayListsEx/src/com/example/ArrayListEx1.java
@@ -9,14 +9,16 @@ package com.example;
 import java.util.ArrayList;
 
 public class ArrayListEx1 {
-
-   
     public static void main(String[] args) {
         ArrayList<String> al = new ArrayList<>();
         al.add("Amy");
         al.add("Bob");
         al.add("Cindy");
         al.add("David");
+        System.out.println(al + " the size of an array is: " + al.size());
+        String temp = al.get(1);
+        al.set(1, al.get(3));
+        al.set(3, temp);
         System.out.println(al + " the size of an array is: " + al.size());
         al.add(0, "Nick");
         al.add(1,"Mike");
